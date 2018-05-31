c0183de65253e8c7466db51d1db42de3b4375072
Percyjackson20 <sathiyavishnu091@gmail.com>
2018-05-31 15:25:33 +0530

    Rectangle area and perimeter finding

--- src/com/emp/Rectangle.java
+++ src/com/emp/Rectangle.java
@@ -5,51 +5,50 @@ public class Rectangle {
 	double width;
 	public Rectangle()
 	{
-		length=1.0;
-		width=1.0;
+		
 	}
 	public double getLength() {
 		return length;
 	}
 	public double setLength(double length) {
+		if(length>0.0&&length<20.0)
+		{
 		this.length = length;
 		return length;
+		}
+		else
+		{
+			return 0.0;
+		}
 	}
 	public double getWidth() {
 		return width;
 	}
 	public double setWidth(double width) {
+		if(width>0.0&&width<20.0)
+		{
 		this.width = width;
 		return width;
+		}
+		else
+		{
+		return 0.0;
+		}
 	}
 	public double area()
 	{
-		if(width>0.0&&width<20.0&&length>0.0&&length<20.0)
-		{
 		double a=length*width;
 		return a;
-		}
-		else
-		{
-		return 0.0;
-		}
 	}
 	public double perimeter()
 	{
-		if(width>0.0&&width<20.0&&length>0.0&&length<20.0)
-		{
 		double p=(length+width)*2;
 		return p;
-		}
-		else
-		{
-		return 0.0;
-		}
 	}
 	public static void main(String a[])
 	{
 		Rectangle r=new Rectangle();
-		r.setLength(7.0);
+		r.setLength(2.0);
 		r.setWidth(8.0);
 		System.out.println("area="+r.area());
 		System.out.println("perimeter="+r.perimeter());
