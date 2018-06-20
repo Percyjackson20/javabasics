13c983cbc07d7d68be1883cbcf8dfc94920dd20e
Percyjackson20 <39692967+Percyjackson20@users.noreply.github.com>
2018-06-20 12:24:41 +0530

    First and Second Minimum in array

---
+++ Minimax.java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+
+public class Minimax
+
+{
+
+
+static int N=0;
+
+public static void main(String args[])
+
+
+{
+
+
+Scanner sc=new Scanner(System.in);
+
+
+N=sc.nextInt();
+
+
+int a[]=new int[N]; 
+
+for(int i=0;i<N;i++)
+
+
+{
+
+
+a[i]=sc.nextInt();
+
+
+}
+
+
+Find(a);
+
+
+}
+
+
+public static void Find(int[] a)
+
+
+{
+
+int m1=Integer.MIN_VALUE;
+    
+int m2=Integer.MIN_VALUE;
+
+for(int n:a)
+
+{
+
+  if(m1<n)
+  
+  {
+
+      m2=m1;
+   
+      m1=n;
+
+  }
+ 
+ else if(m2<n&&m1!=n)
+ 
+  {
+
+      m2=n;
+
+  }
+
+}
+
+System.out.println(m1+" "+m2);
+
+}
+
+}
